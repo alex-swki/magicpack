e5f190af564f8b1ca6c6c7b1cebb8d3a453df96b
Alexander Scharkowski <alex@a13x.cloud>
2023-10-16 23:03:27 +0200

    Added content to page "/"

--- magicpack-app/app/page.tsx
+++ magicpack-app/app/page.tsx
@@ -7,6 +7,15 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <Logo imagewidth={240} />
+      <section className={styles.description}>
+        <h1>
+          Great! Everything is working.. <br />
+          <br />
+          Or is it?
+        </h1>
+        <p>{process.env.NEXT_PUBLIC_PAGE_DESC1}</p>
+        <p>{process.env.NEXT_PUBLIC_PAGE_DESC2}</p>
+      </section>
       <PoweredBy />
     </main>
   );
